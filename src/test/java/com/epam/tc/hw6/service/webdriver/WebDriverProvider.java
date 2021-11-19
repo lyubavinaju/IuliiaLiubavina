630070bb6ba6a347d3c80aaa18cdf47e39df7e94
Liubavina Iuliia <liubavinaiuliia@gmail.com>
2021-11-19 13:46:07 +0300

    hw6

--- src/test/java/com/epam/tc/hw6/service/webdriver/WebDriverProvider.java
+++ src/test/java/com/epam/tc/hw6/service/webdriver/WebDriverProvider.java
@@ -4,6 +4,9 @@ import java.util.Locale;
 import org.openqa.selenium.WebDriver;
 
 public final class WebDriverProvider {
+
+    public static final String BROWSER_NAME_KEY = "browser.name";
+    public static final String DEFAULT_BROWSER_NAME = "firefox";
     private static WebDriver driver;
 
     private WebDriverProvider() {
@@ -11,7 +14,7 @@ public final class WebDriverProvider {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            String browserName = System.getProperty("browser.name", "firefox");
+            String browserName = System.getProperty(BROWSER_NAME_KEY, DEFAULT_BROWSER_NAME);
             Browser browser = Browser.valueOf(browserName.toUpperCase(Locale.ROOT));
             driver = WebDriverFactory.createWebDriver(browser);
         }
